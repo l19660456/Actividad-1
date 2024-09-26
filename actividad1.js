8eabf3ff53fafa0ccfd1e77d968f8f7984ac57eb
l19660456 <l19660456@matehuala.tecnm.mx>
2024-09-26 00:19:27 -0600

    first commit

---
+++ actividad1.js
@@ -0,0 +1,5 @@
+// Cuando la página se cargue, ejecuta esta función para mostrar el mensaje
+window.onload = function() {
+    var mensaje = "¡No me gusta la programación!";
+    document.getElementById("soyelid").innerText = mensaje;
+};
